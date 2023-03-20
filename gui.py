d3050068116274e454ddf4fd007c92afec9c61f8
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-03-20 05:57:14 +0530

    ♻

--- gui.py
+++ gui.py
@@ -4,24 +4,22 @@ import matrix_detection
 from models.configuration import Config
 
 
-def nothing(x):
-    pass
-
-
-def main():
+def main(retries):
     config = Config.load_config()
     cap = cv.VideoCapture(0)
+    if not cap.grab():
+        print("Video Not working")
+        print("Retrying")
+        return main(retries -1)
+    elif not retries:
+        print("Done retrying")
+        return None
+
     cap.set(cv.CAP_PROP_FPS, 30)
     cap.set(cv.CAP_PROP_AUTO_EXPOSURE, 3)
 
-    # cv.namedWindow("image")
-    # cv.createTrackbar('blur_size', 'image', 0, 255, nothing)
-    # cv.createTrackbar('area_max', 'image', 100_000, 100_000_000, nothing)
-    # cv.createTrackbar('area_min', 'image', 100, 100_000, nothing)
     while True:
         ret, frame = cap.read()
-        # config.area_max = cv.getTrackbarPos('area_max', "image")
-        # config.area_min = cv.getTrackbarPos('area_min', "image")
 
         decoded = matrix_detection.decode_matrix(frame, config)
         cv.imshow("image", decoded["final"])
@@ -34,4 +32,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(3)
